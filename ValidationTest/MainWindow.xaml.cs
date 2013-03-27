4389f12ee9d721d863dcbf059b89eb986a2cc161
Wenda Zhou <zhouwenda@outlook.com>
2013-03-27 02:24:35 +0100

    add sample for validation

---
+++ ValidationTest/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+using ReactiveUI.Xaml;
+
+namespace ValidationTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IViewFor<ViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModel = new ViewModel();
+
+            this.Bind(ViewModel, x => x.MyValidatedProperty, v => v.MyTextBox.Text);
+            this.DisplayValidationFor(x => x.MyTextBox.Text);
+
+            this.Bind(ViewModel, x => x.NestedViewModel.MyNotifyProperty, v => v.MySecondTextBox.Text);
+            this.DisplayValidationFor(x => x.MySecondTextBox.Text);
+        }
+
+
+        #region ViewModel
+        public ViewModel ViewModel
+        {
+            get { return (ViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ViewModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(ViewModel), typeof(MainWindow), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (ViewModel)value; }
+        }
+        #endregion
+
+        void SetError_OnClick(object sender, RoutedEventArgs e)
+        {
+            ViewModel.NestedViewModel.HasErrors = true;
+        }
+
+        void ClearError_OnClick(object sender, RoutedEventArgs e)
+        {
+            ViewModel.NestedViewModel.HasErrors = false;
+        }
+    }
+}
---
+++ ValidationTest/ViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace ValidationTest
+{
+    public class ViewModel : ReactiveValidatedObject 
+    {
+        string myValidatedProperty;
+        readonly ViewModel2 nestedViewModel = new ViewModel2();
+
+        [Required]
+        [EmailAddress]
+        public string MyValidatedProperty
+        {
+            get { return myValidatedProperty; }
+            set { this.RaiseAndSetIfChanged(ref myValidatedProperty, value); }
+        }
+
+        public ViewModel2 NestedViewModel { get { return nestedViewModel; } }
+    }
+
+    public class ViewModel2 : INotifyDataErrorInfo
+    {
+        bool hasErrors;
+
+        public string MyNotifyProperty { get; set; }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return HasErrors ? new[] {"My Error"} : Enumerable.Empty<object>();
+        }
+
+        public bool HasErrors
+        {
+            get { return hasErrors; }
+            set
+            {
+                if (hasErrors != value) {
+                    hasErrors = value;
+                    OnErrorsChanged(new DataErrorsChangedEventArgs("MyNotifyProperty"));
+                }
+            }
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e)
+        {
+            var handler = ErrorsChanged;
+            if (handler != null) handler(this, e);
+        }
+    }
+}
